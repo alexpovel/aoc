64fabea3269b6b0c3821b5a4a6a3bf795324ac4f
Alex Povel <git@alexpovel.de>
2023-12-06 22:28:08 +0100

    Add Day 5 Part 1

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -2,6 +2,7 @@ pub mod day01;
 pub mod day02;
 pub mod day03;
 pub mod day04;
+pub mod day05;
 pub mod day06;
 pub mod iter;
 
@@ -33,6 +34,7 @@ fn main() {
         Box::new(day03::part2::Part {}),
         Box::new(day04::part1::Part {}),
         Box::new(day04::part2::Part {}),
+        Box::new(day05::part1::Part {}),
         Box::new(day06::part1::Part {}),
         Box::new(day06::part2::Part {}),
     ];
